ab53707be934ca5416760edb1b9664ba5be1b059
Bop <collin.cannavo@gmail.com>
2017-07-30 22:06:38 -0600

    adding to GitHub

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by Caleb Hicks on 9/23/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    
+    var isOn: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        if isOn {
+            self.isOn = false
+            
+            self.button.setTitle("On", for: .normal)
+            self.button.setTitleColor(UIColor.white, for: .normal)
+            self.view.backgroundColor = UIColor.black
+            
+        } else {
+            self.isOn = true
+            
+            self.button.setTitle("Off", for: .normal)
+            self.button.setTitleColor(UIColor.black, for: .normal)
+            self.view.backgroundColor = UIColor.white
+        }
+    
+    }
+  
+}
+
